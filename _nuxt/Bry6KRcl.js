b6d642336f638dfd4f42e03f53a3981c3ce4ed71
Object-7 <62817076+Object-7@users.noreply.github.com>
2025-04-18 16:38:15 +0200

    test nuxt static page

---
+++ _nuxt/Bry6KRcl.js
@@ -0,0 +1,1 @@
+import{_ as o,c as r,p as t,o as n}from"./CDyKDSHM.js";const s={};function c(e,a){return n(),r("strong",null,[t(e.$slots,"default")])}const _=o(s,[["render",c]]);export{_ as default};
