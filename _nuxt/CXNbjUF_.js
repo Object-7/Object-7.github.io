bf1e46e6341b0b25ab44db3bb02be8753a631ce4
Object-7 <62817076+Object-7@users.noreply.github.com>
2025-04-18 16:50:46 +0200

    try fix colorPicker

---
+++ _nuxt/CXNbjUF_.js
@@ -0,0 +1,1 @@
+import{_ as o,c as t,p as c,o as r}from"./ymgPNH3z.js";const s={};function n(e,l){return r(),t("blockquote",null,[c(e.$slots,"default")])}const _=o(s,[["render",n]]);export{_ as default};
